4dfdcf83f93f40464d7cae318abe491d15ce9af8
Reykart <80640845+Reykart@users.noreply.github.com>
2023-09-21 06:32:28 +0300

    Update get_order_by_track_test.py
    Убрал лишний комментарий

--- get_order_by_track_test.py
+++ get_order_by_track_test.py
@@ -7,5 +7,3 @@ def test_get_order_by_track():
     track = sender_stand_request.create_new_order(data.order_body).json()["track"]  # получаем трек
     response = sender_stand_request.get_order_by_track(track)  # вызываем функцию получения заказа по треку
     assert response.status_code == 200  # убеждаемся, что код ответа 200
-
-# Андрей Карташов, 8-я когорта — Финальный проект. Инженер по тестированию плюс
